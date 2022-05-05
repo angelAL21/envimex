173b10bca9f158441eb74c07b13db7e47e71bf66
unknown <angell_2104@hotmail.com>
2022-05-05 07:49:38 -0500

    agregado el path para buscar por el id del municipio

--- envioinfo_app/urls.py
+++ envioinfo_app/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from envioinfo_app.views import info_list
+from envioinfo_app.views import info_list, municipio_detail
 
 #url para mostrar el lsitado de los municipios.
 urlpatterns = [
     path('listado/', info_list, name='municipio-listado'),
+    path('<int:pk>', municipio_detail, name='municipio-detalle'),
 ]
