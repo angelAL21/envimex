50abeee5b1cadda98c8249662e546432fdb65422
unknown <angell_2104@hotmail.com>
2022-05-05 09:50:55 -0500

    actualizando y guardando los datos si se modifican los datos

--- envioinfo_app/api/serializers.py
+++ envioinfo_app/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from envioinfo_app.models import Info 
 
 #serializer para la info view y manejo más rápido de los datos.
 class InfoSerializer(serializers.Serializer):
@@ -7,3 +8,16 @@ class InfoSerializer(serializers.Serializer):
     estado = serializers.CharField()
     colonia = serializers.CharField()
     cp = serializers.CharField()
+    
+    def create(self, validated_data):
+        return Info.objects.create(**validated_data)
+    
+    
+    #actualizando los datos de cada municipio
+    def update(self, instance, validated_data):
+        instance.municipio = validated_data.get('municipio', instance.municipio)
+        instance.estado = validated_data.get('estado', instance.estado)
+        instance.colonia = validated_data.get('colonia', instance.colonia)
+        instance.cp = validated_data.get('cp', instance.cp)
+        instance.save()
+        return instance
