f203b78f895e8b1a46b730158b565ee67c4f6471
unknown <angell_2104@hotmail.com>
2022-05-05 09:15:54 -0500

    se ha actualizado el path municpios por la carpeta api de envioinfo_app

--- envimex/urls.py
+++ envimex/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('municipio/', include('envioinfo_app.urls')),
+    path('municipio/', include('envioinfo_app.api.urls')),
 ]
